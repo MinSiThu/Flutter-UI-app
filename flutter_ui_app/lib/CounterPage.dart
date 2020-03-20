e3cfe051996873a9891277e6e23b6e669fbbaa94
Min Si Thu <archimedes557@gmail.com>
2020-03-20 16:15:38 +0630

    Update CounterPage.dart

--- flutter_ui_app/lib/CounterPage.dart
+++ flutter_ui_app/lib/CounterPage.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CounterPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text("Counter"),),
-      body: Container(child: Column(
+class CounterPage extends StatefulWidget {
+  const CounterPage({ Key key }) : super(key: key);
 
-      ),),
-      );
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<CounterPage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Counter",
+      home:  Scaffold(
+        appBar: AppBar(title: Text("Counter App"),),
+        body: Container(child: Column(
+          children: <Widget>[
+            Text("${_counter}")
+          ],
+        ),),
+      ),
+    );
   }
 }
